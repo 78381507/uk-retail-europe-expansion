0f0a5dfbe9c13528683dca4d74f0b1a6122406e0
Francois TILKIN <tilkinfrancois@hotmail.com>
2025-12-27 23:10:22 +0100

    Update dim_products.sql

--- File04_SQL/02_data_models/dim_products.sql
+++ File04_SQL/02_data_models/dim_products.sql
@@ -1,1 +1,10 @@
-
+-- Table dimension products
+CREATE OR REPLACE TABLE `uk-retail-europe.mart.dim_products` AS
+SELECT 
+  StockCode,
+  Description,
+  AVG(UnitPrice) AS AvgPrice,
+  COUNT(DISTINCT InvoiceNo) AS TimesOrdered
+FROM `uk-retail-europe.clean.online_retail`
+WHERE StockCode IS NOT NULL
+GROUP BY StockCode, Description;
