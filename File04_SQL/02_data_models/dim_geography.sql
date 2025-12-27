63e458edd58a1fedb4f7b6bf5129ae97bcd675e7
Francois TILKIN <tilkinfrancois@hotmail.com>
2025-12-27 23:09:31 +0100

    Update dim_geography.sql

--- File04_SQL/02_data_models/dim_geography.sql
+++ File04_SQL/02_data_models/dim_geography.sql
@@ -1,1 +1,12 @@
-
+-- Table dimension geography
+CREATE OR REPLACE TABLE `uk-retail-europe.mart.dim_geography` AS
+SELECT DISTINCT
+  Country,
+  CASE 
+    WHEN Country IN ('France', 'Belgium', 'Switzerland') THEN 'Francophone'
+    WHEN Country = 'United Kingdom' THEN 'UK'
+    WHEN Country IN ('Germany', 'Spain', 'Italy', 'Netherlands', 'Portugal') THEN 'Europe West'
+    ELSE 'Other'
+  END AS Region
+FROM `uk-retail-europe.clean.online_retail`
+WHERE Country IS NOT NULL;
