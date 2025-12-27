7d157eedd513263a961058bbd821d91ff68d156d
Francois TILKIN <tilkinfrancois@hotmail.com>
2025-12-27 23:11:01 +0100

    Update fact_orders.sql

--- File04_SQL/02_data_models/fact_orders.sql
+++ File04_SQL/02_data_models/fact_orders.sql
@@ -1,1 +1,17 @@
-
+-- Table de faits - orders
+CREATE OR REPLACE TABLE `uk-retail-europe.mart.fact_orders` AS
+SELECT 
+  InvoiceNo,
+  InvoiceDate,
+  CustomerID,
+  StockCode,
+  Country,
+  Quantity,
+  UnitPrice,
+  TotalAmount,
+  IsReturn,
+  IsB2B,
+  EXTRACT(YEAR FROM InvoiceDate) AS Year,
+  EXTRACT(MONTH FROM InvoiceDate) AS Month,
+  FORMAT_TIMESTAMP('%Y-%m', InvoiceDate) AS YearMonth
+FROM `uk-retail-europe.clean.online_retail`;
