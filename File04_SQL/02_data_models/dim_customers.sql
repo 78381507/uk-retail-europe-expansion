7efa30dc1c5357c1dcf4eec3cd12260bcb0b3484
Francois TILKIN <tilkinfrancois@hotmail.com>
2025-12-27 23:10:01 +0100

    Update dim_customers.sql

--- File04_SQL/02_data_models/dim_customers.sql
+++ File04_SQL/02_data_models/dim_customers.sql
@@ -1,1 +1,12 @@
-
+-- Table dimension customers
+CREATE OR REPLACE TABLE `uk-retail-europe.mart.dim_customers` AS
+SELECT 
+  CustomerID,
+  Country,
+  MIN(InvoiceDate) AS FirstPurchaseDate,
+  MAX(InvoiceDate) AS LastPurchaseDate,
+  COUNT(DISTINCT InvoiceNo) AS TotalOrders,
+  SUM(TotalAmount) AS TotalRevenue
+FROM `uk-retail-europe.clean.online_retail`
+WHERE CustomerID IS NOT NULL
+GROUP BY CustomerID, Country;
